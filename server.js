3158bc0150055e95b79925545f89a12914b7890f
Oxair <pk.ozair@gmail.com>
2019-09-30 01:53:09 -0700

    Stage 1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express = require('express')
 let App = express()
 
+App.use(express.urlencoded({extended: false}))
+
 App.get('/', function(req, res){
     res.send(`<!DOCTYPE html>
     <html>
@@ -15,9 +17,9 @@ App.get('/', function(req, res){
         <h1 class="display-4 text-center py-1">To-Do App</h1>
         
         <div class="jumbotron p-3 shadow-sm">
-          <form>
+          <form action='/create-item' method='POST'>
             <div class="d-flex align-items-center">
-              <input autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <input name='item' autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
               <button class="btn btn-primary">Add New Item</button>
             </div>
           </form>
@@ -51,6 +53,11 @@ App.get('/', function(req, res){
       
     </body>
     </html>`)
+})
+
+App.post('/create-item', function(req, res){
+    console.log(req.body.item)
+    res.send('Thankyou for submitting the form..')
 })
 
 App.listen(3000)

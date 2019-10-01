540a15b8bc80304695c864acdc6fc1c766031cc8
Oxair <pk.ozair@gmail.com>
2019-10-01 03:20:34 -0700

    Stage 3

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongodb.connect(connectionString, { useNewUrlParser: true }, function(err, clien
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
+    <title>Node, Express, Axios - TD App</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     </head>
     <body>
@@ -32,21 +32,21 @@ mongodb.connect(connectionString, { useNewUrlParser: true }, function(err, clien
     <h1 class="display-4 text-center py-1">To-Do App!</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-    <form action="/create-item" method="POST">
+    <form id="create-form" action="/create-item" method="POST">
     <div class="d-flex align-items-center">
-    <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+    <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
     <button class="btn btn-primary">Add New Item</button>
     </div>
     </form>
     </div>
     
-    <ul class="list-group pb-5">
+    <ul id="item-list" class="list-group pb-5">
       ${items.map(function(item) {
         return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
         <span class="item-text">${item.text}</span>
         <div>
         <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-        <button class="delete-me btn btn-danger btn-sm">Delete</button>
+        <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
         </div>
         </li>`
       }).join('')}
@@ -62,13 +62,19 @@ mongodb.connect(connectionString, { useNewUrlParser: true }, function(err, clien
   })
   
   app.post('/create-item', function(req, res) {
-    db.collection('items').insertOne({text: req.body.item}, function() {
-      res.redirect('/')
+    db.collection('items').insertOne({text: req.body.text}, function(err, info) {
+      res.json(info.ops[0])
     })
   })
   
   app.post('/update-item', function(req, res) {
     db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+      res.send("Success")
+    })
+  })
+
+  app.post('/delete-item', function(req, res){
+    db.collection('items').deleteOne({_id: new mongodb.ObjectID(req.body.id)}, function(){
       res.send("Success")
     })
   })
